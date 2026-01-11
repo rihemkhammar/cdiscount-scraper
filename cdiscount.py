494df3d02ac4d327243c0a2e5d9d7afa051b0bde
rihemkhammar <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>
2026-01-11 20:56:20 +0100

    Data extraction

--- cdiscount.py
+++ cdiscount.py
@@ -1,13 +1,13 @@
-"""- fonction pour changer les critéres (filtre)
+"""x - fonction pour changer les critéres (filtre)
 - fonction pour le loop Item
 - fonction pour l'extraction des donnèes
 - fonction pour exporter sous Excel
 - fonction pour la pagination
 """
 import logging
-from fileinput import filename
+import pandas as pd
+from playwright.sync_api import  TimeoutError as PlaywrightTimeoutError
 
-from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
 
 logging.basicConfig(level=logging.INFO,
                     filename="cdiscount.log",
@@ -15,84 +15,169 @@ logging.basicConfig(level=logging.INFO,
                     format= "%(asctime)s - %(levelname)s - %(message)s",
                     datefmt="%d-%b-%y | %H:%M:%S"
 )
-def change_criterias(page, url):
+
+def load_selectors(excel_path):
+
+    df = pd.read_excel(excel_path)
+
+    required = {"name", "xpath", "url_site"}
+    if not required.issubset(df.columns):
+        raise ValueError("Le fichier Excel doit contenir : name | xpath | url_site")
+
+    return df
+
+def click_by_name(page, df, name, timeout=10000):
+
+    row = df[df["name"] == name]
+    url = df.iloc[0]["url_site"]
     page.goto(url)
-    #Acepter les cookies
+
+    if row.empty:
+        raise ValueError(f"Sélecteur '{name}' introuvable dans selectors.xlsx")
+
+    xpath = row.iloc[0]["xpath"]
+
     try:
-        page.wait_for_selector('xpath=//button[@id="footer_tc_privacy_button_2"]', timeout=10000)
-        page.locator('xpath=//button[@id="footer_tc_privacy_button_2"]').click()
+        page.wait_for_selector(f"xpath={xpath}", timeout=timeout)
+        page.locator(f"xpath={xpath}").click()
+        logging.info(f"Click OK : {name}")
     except PlaywrightTimeoutError as e:
-        logging.error(f"le bouton 'Accepter les cookies n'existe pas':{e}")
-        raise
-    #Cliquer sur 'Tout Supprimer'
-    try:
-        page.wait_for_selector('xpath=/html/body/div[3]/div/section/div[1]/div[1]/div[1]/section[1]/div[1]/button/span', timeout=10000)
-        page.locator('xpath=/html/body/div[3]/div/section/div[1]/div[1]/div[1]/section[1]/div[1]/button/span').click()
-    except PlaywrightTimeoutError as e:
-        logging.error(f"le bouton 'Tout Supprimer  n'existe pas':{e}")
-        raise
-    # Cliquer sur 'taille'
-    try:
-       page.wait_for_selector(
-                'xpath=/html/body/div[3]/div/section/div[1]/div[1]/div[1]/section[1]/section[5]/label',
-                timeout=10000)
-       page.locator('xpath=/html/body/div[3]/div/section/div[1]/div[1]/div[1]/section[1]/section[5]/label').click()
-    except PlaywrightTimeoutError as e:
-        logging.error(f" le bouton taille  n'existe pas:{e}")
+        logging.error(f"Impossible de cliquer sur {name} : {e}")
         raise
 
-        # Cliquer sur '12 pouces'
+
+def get_text_by_name(page, df, name, timeout=10000):
+    row = df[df["name"] == name]
+    if row.empty:
+        raise ValueError(f"Sélecteur '{name}' introuvable dans le DataFrame")
+
+    xpath = row.iloc[0]["xpath"]
+
     try:
-         page.wait_for_selector('xpath=/html/body/div[3]/div/section/div[1]/div[1]/div[1]/section[1]/section[5]/div/div/label[1]', timeout=50000)
-         page.locator(
-                'xpath=/html/body/div[3]/div/section/div[1]/div[1]/div[1]/section[1]/section[5]/div/div/label[1]').click()
+        page.wait_for_selector(f"xpath={xpath}", timeout=timeout)
+        element = page.locator(f"xpath={xpath}")
+        text = element.text_content().strip()
+        logging.info(f"Récupération OK : {name} -> {text}")
+        return text
     except PlaywrightTimeoutError as e:
-        logging.error(f" le bouton 12 pouces  n'existe pas:{e}")
+        logging.error(f"Impossible de récupérer le texte de '{name}' : {e}")
         raise
 
-    # Verification des changement
-    # Extraire le nombre de filtre
+def get_count_by_name(page, df, name, timeout=10000):
+
+    row = df[df["name"] == name]
+    if row.empty:
+        raise ValueError(f"Sélecteur '{name}' introuvable dans le DataFrame")
+
+    xpath = row.iloc[0]["xpath"]
 
     try:
-        nbr_filtre =  page.locator('xpath=/html/body/div[3]/div/section/div[1]/div[1]/div[1]/section[1]/div[2]').locator('xpath=./*').count()
+        page.wait_for_selector(f"xpath={xpath}", timeout=timeout)
+        element = page.locator(f"xpath={xpath}")
+        count = element.locator('xpath=./*').count()  # compte tous les enfants directs
+        logging.info(f"Count OK : {name} -> {count}")
+        return count
     except PlaywrightTimeoutError as e:
-        logging.error(f"le nombre de filtre  n'existe pas:{e}")
+        logging.error(f"Impossible de récupérer le count de '{name}' : {e}")
         raise
-    # Extraire la catregorie
+def get_all(page, df, name, timeout=10000):
+
+    row = df[df["name"] == name]
+    if row.empty:
+        raise ValueError(f"Sélecteur '{name}' introuvable dans le DataFrame")
+
+    xpath = row.iloc[0]["xpath"]
 
     try:
-        catregories = page.locator(
-                'xpath=/html/body/div[3]/div/section/div[1]/div[1]/div[1]/section[1]/div[2]/button/span[1]').text_content().strip()
+        page.wait_for_selector(f"xpath={xpath}", timeout=timeout)
+        element = page.locator(f"xpath={xpath}")
+        items = element.locator('xpath=./*').all()  # compte tous les enfants directs
+        logging.info(f"Count OK : {name} -> {len(items)}")
+        return items
     except PlaywrightTimeoutError as e:
-         logging.error(f"la catregorie n'existe pas:{e}")
-         raise
-
-
-   # Extraire le nombre de produit
-    try:
-        nbr_produit = page.locator(
-            'xpath=/html/body/div[3]/div/section/div[1]/div[1]/div[1]/section[1]/section[5]/div/div/label[1]/span[2]/span').text_content().strip()
-    except PlaywrightTimeoutError as e:
-        logging.error(f"le nombre de produit n'existe pas:{e}")
+        logging.error(f"Impossible de récupérer le count de '{name}' : {e}")
         raise
-    if "1" not in nbr_produit or "12 pouces" not in catregories:
-        logging.error("la catregorie ou le nombre de filtre  n'est pas le bon.")
-        raise ValueError
-    logging.info(f"il ya {nbr_filtre} la catégorie  {catregories} contient {nbr_produit} produit")
-
-    return page
 
 
-def main():
-    url = "https://www.cdiscount.com/informatique/r-pc+portable+14+pouces.html?nav_menu=227::PC%20Portable%2012%22%20%C3%A0%2014%22"
-    with sync_playwright() as playwright:
-        browser = playwright.firefox.launch(headless=False)
-        page = browser.new_page()
-        #Changer les filtres
-        change_criterias(page, url)
+def loop_item(page, df):
+    item_data = []
+    items = get_all(page, df, "product-list")
+    current_page = get_count_by_name(page, df, "current_page")
+    nbr_filtre = get_count_by_name(page, df, "category")
 
-        browser.close()
+    logging.info(f"Il y {nbr_filtre} Filtre(s) sur la page {current_page + 1} qui contient {len(items)} éléments.")
+
+    for item in items:
+        # Link
+        link = None
+        link_loc = item.locator('xpath=.//section/a')
+        if link_loc.count() > 0:
+            link = link_loc.first.get_attribute("href")
+
+        # Title
+        title = None
+        title_loc = item.locator("xpath=.//h2[@data-e2e='lplr-title']")
+        if title_loc.count() > 0:
+            title = title_loc.first.text_content().strip()
+
+        # Ancien prix
+        ancien_prix = None
+        ancien_prix_loc = item.locator('xpath=.//div[@class="sc-83lijy-1 hPUKzb SecondaryPrice-wrapper"]')
+        if ancien_prix_loc.count() > 0:
+            ancien_prix = ancien_prix_loc.first.text_content().strip()
+
+        # Prix actuel
+        prix_actuel = None
+        prix_actuel_loc = item.locator('xpath=.//span[@class="sc-e4stwg-1 gIpMGn"]')
+        if prix_actuel_loc.count() > 0:
+            prix_actuel = prix_actuel_loc.first.text_content().strip()
+
+        # Ajouter au résultat
+        product_details = {
+            "url": link,
+            "title": title,
+            "prix_actuel": prix_actuel,
+            "ancien_prix": ancien_prix
+        }
+        item_data.append(product_details)
+
+    # Affiche les résultats pour debug
+    print(item_data)
+    return item_data
 
 
-if __name__ == '__main__':
-    main()
+
+
+
+
+def change_criterias(page, df):
+
+
+    # --- Cliquer sur les éléments dans l'ordre ---
+    click_by_name(page, df, "cookies_button", timeout=10000)
+    click_by_name(page, df, "delete_all_filtre", timeout=50000)
+    click_by_name(page, df, "taille_label", timeout=50000)
+    click_by_name(page, df, "douze_pouces", timeout=500000)
+
+    # --- Récupérer texte et count ---
+    try:
+        categorie = get_text_by_name(page, df, "category",50000)
+        nbr_produit = get_text_by_name(page, df, "number_products")
+        nbr_filtre = get_count_by_name(page, df, "category")  # 'category' = nom dans Excel
+    except Exception as e:
+        logging.error(f"Erreur lors de la récupération des données : {e}")
+        raise
+
+    if "1" not in nbr_produit or 'Moins de 12"' not in categorie:
+        logging.error("La catégorie ou le nombre de filtre n'est pas le bon.")
+        raise ValueError("Filtres ou catégorie incorrects")
+
+    logging.info(f"Il y a {nbr_filtre} filtre(s), la catégorie '{categorie}' contient {nbr_produit} produit(s).")
+
+    # Retourner un dictionnaire avec toutes les infos
+    return {
+        "categorie": categorie,
+        "nbr_produit": nbr_produit,
+        "nbr_filtre": nbr_filtre
+    }
+
--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
+import selectors
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from playwright.sync_api import sync_playwright
+from cdiscount import load_selectors, click_by_name, get_text_by_name, get_count_by_name, change_criterias, loop_item
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    item_data=[]
+
+    with sync_playwright() as playwright:
+        browser = playwright.firefox.launch(headless=False)
+        page = browser.new_page()
+        # Charger les sélecteurs
+        df = load_selectors("selectors.xlsx")
+
+        # Récupérer l’URL depuis Excel
+        url = df.iloc[0]["url_site"]
+
+        # Navigation
+        page.goto(url)
+
+        # Appliquer les critères
+        info = change_criterias(page, df)
+        item_data = loop_item(page, df)
+        #return page
+        browser.close()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
